9df1fb05505e3fd26b422860b98ca390c0a9ac8c
Hemant Sachdeva <66035497+hemantsachdeva@users.noreply.github.com>
2020-07-06 15:05:05 +0530

    Update generate_session_file.py

--- generate_session_file.py
+++ generate_session_file.py
@@ -1,24 +1,14 @@
-# Copyright (C) 2019 The Raphielscape Company LLC.
-#
-# Licensed under the Raphielscape Public License, Version 1.b (the "License");
-# you may not use this file except in compliance with the License.
-#
-# This script wont run your bot, it just generates a session.
-
 from telethon import TelegramClient
-from dotenv import load_dotenv
-import os
+from telethon.sessions import StringSession
 
 print("""Aa gaye Betichod string generate karne. 
 lodu use ==>> my.telegram.com (vpn use karna) <<==
 Apna Telegram Account login kar btc
 Click on API Development Tools Blah Blah Maa ki chu
-Create a new application, by entering the required details Maa ka bhosda""")
+Create a new application, by entering the required details of teri Maa ka bhosda""")
 
-load_dotenv("config.env")
 
-API_KEY = int(input("Madarchod APP ID Daal Behen Ke Lawde: "))
-API_HASH = input("Gandu Khush Matt Ho Ab API HASH daal: ")
-
-bot = TelegramClient('userbot', API_KEY, API_HASH)
-bot.start()
+APP_API_ID = int(input("Madarchod APP API ID Daal Behen Ke Lawde: "))
+APP_API_HASH = input("Gandu Khush Matt Ho Ab APP API HASH daal: ")
+with TelegramClient(StringSession(), APP_API_ID, APP_API_HASH) as client:
+    print(client.session.save())
